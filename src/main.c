8263d9e1c5deddb2152f5c48e1a3b8d5cacc0f12
Roboticol <fiverrticol@gmail.com>
2025-12-30 12:29:45 +0530

    update main.c

--- src/main.c
+++ src/main.c
@@ -1,4 +1,5 @@
 // Made by Roboticol
+// make sure to check out the backprop.h/c, nutils.h/c and lstm.h/c files!
 
 #include <stdio.h>
 #include <gsl/gsl_math.h>
@@ -8,29 +9,35 @@
 #include "backprop.h"
 
 int main() {
-	init_utils();
+	init_utils(); //initialize the utilities library
 
-	int inp_dim = 2;
-	int hidden_dim = 2;
-	int output_dim = 2;
-	LSTM* l = create_rand_lstm(inp_dim, hidden_dim, output_dim,-1,1,-10,10);
-	randomize_in_lstm(l, -1,1);
+	// dimensions
+	int input_dim = 1; // takes just one input
+	int output_dim = 1; // outputs one value
+	int hidden_dim = 3; // hidden state is a vector of 3 elements
+	
+	// initialize an lstm with randomized weights matrices and biases vectors
+	LSTM *lstm = create_rand_lstm(input_dim, hidden_dim, output_dim, -10, 10, -10, 10);
+	// display lstm values
+	print_lstm(lstm);
 
+	// create data for the lstm to train on
+	gsl_vector **series = series_vectors(1, 10, 1, 10, 1, 10); // generates a series of 10 vectors of size 1 and all its values are randomized between 1 and 10
+	// display series data
+	print_series_vectors(series, 10, "\n\nData: ");
 
-	gsl_vector *v = create_rand_vector(output_dim, -10, 10);
-	gsl_vector *h = create_rand_vector(hidden_dim, -10, 10);
-	print_vector(v, "inp: ");
+	// have the lstm predict its result on the series
+	forward_pass_n_lstm(lstm, series, 10);
+	
+	// print lstm after forward pass
+	print_lstm(lstm);
 
-	input_vector_lstm(l, v);
+	// output final value of lstm
+	print_vector(lstm->y, "Output of lstm: ");
 
-	forward_pass_lstm(l);
-	bp_dEdh(l, v, h);
-
-	print_lstm(l);	
-	print_vector(h, "dE/dh: ");
-
-	gsl_vector_free(v);
-	free_lstm(l);
+	// memory management steps
+	free_lstm(lstm);
+	free_series_vectors(series, 10);
 
 	return 0;
 }
